c76263532ee8e316f1b9af4fdb984e743493ac64
Chris <who7708@163.com>
2023-01-08 16:01:40 +0800

    刷新List测试

---
+++ RecyclerView/Application/src/main/java/com/example/android/refresh/Main2Activity.java
@@ -0,0 +1,118 @@
+package com.example.android.refresh;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android.recyclerview.R;
+
+import java.util.ArrayList;
+
+public class Main2Activity extends AppCompatActivity {
+    private static final int UPDATE_DATA = 0x3;
+
+    private RecyclerView rv;
+
+    RvAdapter1 adapter;
+
+    private ArrayList<String> arrayList = new ArrayList<>();
+
+    //加载更多数据时最后一项的索引
+    private int lastLoadDataItemPosition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        rv = (RecyclerView) findViewById(R.id.rv_main2);
+
+        //设置布局管理器
+        rv.setLayoutManager(new LinearLayoutManager(this));//线性
+        // rv.setLayoutManager(new GridLayoutManager(this, 4));//线性
+        // rv.setLayoutManager(new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL));//线性
+        initData();
+        adapter = new RvAdapter1(this, arrayList);
+        adapter.setOnItemClickListener(new RvAdapter1.OnItemClickListener() {
+            @Override
+            public void onItemClick(RecyclerView parent, View view, int position, String data) {
+                Toast.makeText(Main2Activity.this, data, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE &&
+                        lastLoadDataItemPosition == adapter.getItemCount()) {
+                    new LoadDataThread().start();
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+                RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                if (layoutManager instanceof LinearLayoutManager) {
+                    LinearLayoutManager manager = (LinearLayoutManager) layoutManager;
+                    int firstVisibleItem = manager.findFirstVisibleItemPosition();
+                    int l = manager.findLastCompletelyVisibleItemPosition();
+                    lastLoadDataItemPosition = firstVisibleItem + (l - firstVisibleItem) + 1;
+                }
+            }
+        });
+
+        rv.setAdapter(adapter);
+    }
+
+    /**
+     * 初始化数据
+     *
+     * @return
+     */
+    public void initData() {
+        for (int i = 0; i < 25; i++) {
+            arrayList.add("第" + i + "条数据");
+        }
+    }
+
+    /**
+     * 线程模拟加载数据
+     */
+    class LoadDataThread extends Thread {
+        @Override
+        public void run() {
+            initData();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Message message = handler.obtainMessage();
+            message.what = UPDATE_DATA;
+            message.obj = arrayList;
+            handler.sendMessage(message);
+        }
+    }
+
+    private final Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case UPDATE_DATA:
+                    arrayList = (ArrayList<String>) msg.obj;
+                    adapter.setmList(arrayList);
+                    adapter.notifyDataSetChanged();
+                    break;
+            }
+        }
+    };
+}
---
+++ RecyclerView/Application/src/main/java/com/example/android/refresh/RvAdapter1.java
@@ -0,0 +1,179 @@
+package com.example.android.refresh;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android.recyclerview.R;
+
+import java.util.ArrayList;
+
+/**
+ * RecycleView的Adapter
+ */
+public class RvAdapter1 extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements
+        View.OnClickListener {
+    private static final int ITEM_FOOTER = 0x1;
+
+    private static final int ITEM_DATA = 0x2;
+
+    private Context mContext;
+
+    private RecyclerView recyclerView;
+
+    private ArrayList<String> mList;
+
+    public RvAdapter1() {
+    }
+
+    public RvAdapter1(Context mContext, ArrayList<String> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
+    }
+
+    public void setmList(ArrayList<String> mList) {
+        this.mList = mList;
+    }
+
+    /**
+     * 用于创建ViewHolder
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        RecyclerView.ViewHolder vh = null;
+        switch (viewType) {
+            case ITEM_DATA:
+                view = LayoutInflater.from(mContext).inflate(R.layout.test_item, null);
+                view.setOnClickListener(this);
+                vh = new DataViewHolder(view);
+                //使用代码设置宽高（xml布局设置无效时）
+                view.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+                break;
+            case ITEM_FOOTER:
+                view = LayoutInflater.from(mContext).inflate(R.layout.test_item_footer, null);
+                //使用代码设置宽高（xml布局设置无效时）
+                vh = new FooterViewHolder(view);
+                view.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+                break;
+        }
+        return vh;
+    }
+
+    /**
+     * 获取Item的View类型
+     *
+     * @param position
+     * @return
+     */
+    @Override
+    public int getItemViewType(int position) {
+        //根据 Item 的 position 返回不同的 Viewtype
+        if (position == (getItemCount()) - 1) {
+            return ITEM_FOOTER;
+        } else {
+            return ITEM_DATA;
+        }
+    }
+
+    /**
+     * 绑定数据
+     *
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof DataViewHolder) {
+            DataViewHolder dataViewHolder = (DataViewHolder) holder;
+            dataViewHolder.tv_data.setText(mList.get(position));
+        } else if (holder instanceof FooterViewHolder) {
+
+        }
+    }
+
+    /**
+     * 选项总数
+     *
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        return mList.size() + 1;
+    }
+
+    @Override
+    public void onClick(View view) {
+        //根据RecyclerView获得当前View的位置
+        int position = recyclerView.getChildAdapterPosition(view);
+        //程序执行到此，会去执行具体实现的onItemClick()方法
+        if (onItemClickListener != null) {
+            onItemClickListener.onItemClick(recyclerView, view, position, mList.get(position));
+        }
+    }
+
+    /**
+     * 创建ViewHolder
+     */
+    public static class DataViewHolder extends RecyclerView.ViewHolder {
+        TextView tv_data;
+
+        public DataViewHolder(View itemView) {
+            super(itemView);
+            tv_data = (TextView) itemView.findViewById(R.id.tv_recycle);
+        }
+    }
+
+    /**
+     * 创建footer的ViewHolder
+     */
+    public static class FooterViewHolder extends RecyclerView.ViewHolder {
+        public FooterViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    private OnItemClickListener onItemClickListener;
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    /**
+     * 定义RecyclerView选项单击事件的回调接口
+     */
+    public interface OnItemClickListener {
+        //参数（父组件，当前单击的View,单击的View的位置，数据）
+        void onItemClick(RecyclerView parent, View view, int position, String data);
+    }
+
+    /**
+     * 将RecycleView附加到Adapter上
+     */
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        this.recyclerView = recyclerView;
+    }
+
+    /**
+     * 将RecycleView从Adapter解除
+     */
+    @Override
+    public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView);
+        this.recyclerView = null;
+    }
+}
